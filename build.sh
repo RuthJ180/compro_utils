d74ffc8dc9219fb8617c136111bf56f39796ba9e
Brad Czerniak <ao5357@gmail.com>
2016-02-09 18:39:18 -0500

    Build and help for most of the scripts

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+# Put the readme header in place using a copy command.
+cp _docs/intro.md readme.md
+
+# Loop through the commands and call their -h versions.
